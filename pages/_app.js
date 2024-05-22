c19c020c5b3f9722185a0ae96aaacbb5345a9a4e
Julien Arbellini <arbellini.j@gmail.com>
2024-05-22 22:18:06 +0200

    Web Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 // pages/_app.js
-import '../styles/globals.css';
+// pages/_app.js ou pages/_app.tsx
+import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
